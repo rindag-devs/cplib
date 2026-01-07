22a331b77329969e7de2b4aa34b71976aedaddce
Aberter Yan <aberter0x3f@disroot.org>
2026-01-07 10:42:11 +0800

    docs: Fix sitemap

--- docs/next-sitemap.js
+++ docs/next-sitemap.js
@@ -1,5 +1,23 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://cplib.aberter0x3f.top',
+  siteUrl: process.env.SITE_URL || "https://cplib.aberter0x3f.top",
   generateRobotsTxt: true,
+  transform: async (config, path) => {
+    let loc = path;
+
+    if (path.endsWith(".en")) {
+      loc = path.replace(/\.en$/, "");
+    } else if (path.endsWith(".zh")) {
+      const cleanPath = path.replace(/\.zh$/, "");
+      loc = `/zh${cleanPath.startsWith("/") ? cleanPath : "/" + cleanPath}`;
+    }
+
+    return {
+      loc: loc,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
 };
