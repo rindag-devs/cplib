b17e4acae147199388a22f85e89243123daabc3d
Aberter Yan <aberter0x3f@disroot.org>
2026-02-22 11:03:36 +0800

    chore: Fix clangd in nix dev shell

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "cplib";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
